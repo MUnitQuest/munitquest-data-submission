22cd2cce2f751ac9025e8a7006579d91e175ff06
PaulBrandenburg <plbrndnbrg@gmail.com>
2026-05-22 09:11:51 +0200

    principle unit tests for report generation

--- tests/unit/test_report.py
+++ tests/unit/test_report.py
@@ -5,7 +5,7 @@ Test cases for HTML report module.
 import pytest
 import os
 
-from bundle.scoring.report import MUnitQuestDataSubmissionReport
+from bundle.scoring.report import MUnitQuestDataSubmissionReport as Report
 
 
 @pytest.mark.parametrize("valid, expected_status", [
@@ -13,5 +13,44 @@ from bundle.scoring.report import MUnitQuestDataSubmissionReport
     (False, "Invalid")
 ])
 def test_status_property(valid, expected_status):
-    report = MUnitQuestDataSubmissionReport(valid=valid, errors=[], warnings=[])
+    report: Report = Report(valid=valid, errors=[], warnings=[])
     assert report.validation_status == expected_status
+
+
+def test_aggregation():
+    items = [
+        {"code": "E1", "location": "file1"},
+        {"code": "E1", "location": "file2"},
+        {"code": "W1", "location": "file3"},
+    ]
+    counter, grouped = Report.aggregate_by_code(items)
+    assert counter["E1"] == 2
+    assert counter["W1"] == 1
+    assert len(grouped["E1"]) == 2
+    assert len(grouped["W1"]) == 1
+
+
+def test_html_generation_invalid():
+    errors: list[dict] = [
+        {
+            "code": "E1",
+            "location": "file1",
+            "severity": "error"
+        }
+    ]
+    report: Report = Report(valid=False, errors=errors, warnings=[])
+    report.generate_html()
+    assert "<h2>Upload Instructions</h2>" not in report.html
+
+
+def test_html_generation_valid():
+    warnings: list[dict] = [
+        {
+            "code": "E1",
+            "location": "file1",
+            "severity": "warning"
+        }
+    ]
+    report: Report = Report(valid=True, errors=[], warnings=warnings)
+    report.generate_html()
+    assert "<h2>Upload Instructions</h2>" in report.html
