44c5d5b8be25ba0168a74e9b7fb672c09ae419a8
PaulBrandenburg <plbrndnbrg@gmail.com>
2026-06-09 10:44:17 +0200

    controlled stdout/stderr of bash script orchestrating submission workflow

--- bundle/scoring/execute.sh
+++ bundle/scoring/execute.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-echo "Executing scoring program..."
+PIPLOG="/app/output/environment.log"
+
 # installs dependencies
-python3 -m pip install bids-validator-deno
-python3 -m pip install -e .
+printf "Setting up environment (BIDS-Validator & MUniverse)...\n"
+python3 -m venv .submission
+source .submission/bin/activate
+
+python3 -m pip install --upgrade pip --quiet --quiet --log $PIPLOG
+python3 -m pip install bids-validator-deno --quiet --quiet --log $PIPLOG
+# install MUniverse
+git clone https://github.com/dfarinagroup/muniverse.git &>> $PIPLOG
+cd muniverse
+python3 -m pip install -e . --quiet --quiet --log $PIPLOG
+cd ..
+
+printf "Building scoring program...\n"
+python3 -m pip install -e . --quiet --quiet --log $PIPLOG
 
 # run the program
+printf "\nRunning scoring program...\n"
 python3 main.py /app/input/res /app/output/
